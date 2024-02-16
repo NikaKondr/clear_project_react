a84939282e04b8e1b5fd04dc2738650e7e5fcf74
NikaKondr <nika.kondr@yandex.ru>
2024-02-16 14:04:03 +0100

    - upd

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { configPaths } = require( 'react-app-rewire-alias' )
+const { aliasDangerous } = require( 'react-app-rewire-alias/lib/aliasDangerous' );
+
+module.exports = function override ( config ) {
+	config.entry = {
+		main: [
+			require.resolve( './pre-load.ts' ),
+		]
+	};
+	aliasDangerous( {
+		...configPaths( './tsconfig.paths.json' )
+	} )( config )
+
+	return config;
+}
